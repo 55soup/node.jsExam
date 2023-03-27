b384841081738f8b0296c9f600eba28d197cece0
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-03-27 20:00:54 +0900

    폼에 입력한 데이터 서버에 전송 POST요청

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 8081;
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
+app.get("/pet", function (요청, 응답) {
+  응답.send("펫용품 사시오");
+});
+
+app.get("/beauty", function (요청, 응답) {
+  응답.send("뷰티용품 쇼핑 페이지임");
+});
+
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+
+app.get("/write", (요청, 응답) => {
+  응답.sendFile(__dirname + "/write.html");
 });
