ac29b07a1b187f0bdd986b9f3e6417fa377db5f4
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-03-27 20:36:44 +0900

    ejs라이브러리 사용, DB내용가져와서 html에 뿌리기✨

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = 8081;
-const MongoClient = require("mongodb").MongoClient;
+const port = 8081; //서버포트
+const MongoClient = require("mongodb").MongoClient; //mogoDB사용
+app.set("view engine", "ejs"); //ejs nodejs가 렌더링 할 수 있도록 하는 코드
 
 let db;
 MongoClient.connect(
@@ -46,17 +47,23 @@ app.post("/add", function (요청, 응답) {
   응답.send("전송완료");
   console.log(요청.body.date);
   console.log(요청.body.title);
-  MongoClient.connect(
-    "mongodb+srv://admin:qwer1234@cluster0.u0flyy5.mongodb.net/todoapp?retryWrites=true&w=majority",
-    function (에러, client) {
-      if (에러) return console.log(에러);
-      db = client.db("todoapp");
-      db.collection("post").insertOne(
-        { 제목: 요청.body.title, 날짜: 요청.body.date },
-        function (error, result) {
-          console.log("저장완료");
-        }
-      );
+  db.collection("post").insertOne(
+    { 제목: 요청.body.title, 날짜: 요청.body.date },
+    function (error, result) {
+      console.log("저장완료");
     }
   );
+});
+
+app.get("/list", (request, response) => {
+  // DB에서 data를 꺼낸 후
+  db.collection("post")
+    .find()
+    .toArray(function (error, result) {
+      console.log(result);
+      response.render("list.ejs", { posts: result });
+    });
+
+  //HTML렌더링
+  // DB에 저장된 post라는 collection안에 모든 데이터를 꺼내기
 });
