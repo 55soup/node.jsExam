658e9f89b3716f0bbc8413c88a6f20a2d0bb1a2b
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-04-10 11:42:13 +0000

    Add: detail page 만들기

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = 8081; //서버포트
 const MongoClient = require("mongodb").MongoClient; //mogoDB사용
 app.set("view engine", "ejs"); //ejs nodejs가 렌더링 할 수 있도록 하는 코드
+app.use("/public", express.static("public"));
 
 let db;
 MongoClient.connect(
@@ -26,25 +27,25 @@ MongoClient.connect(
   }
 );
 
-app.get("/pet", function (요청, 응답) {
-  응답.send("펫용품 사시오");
+app.get("/pet", function (req, res) {
+  res.send("펫용품 사시오");
 });
 
-app.get("/beauty", function (요청, 응답) {
-  응답.send("뷰티용품 쇼핑 페이지임");
+app.get("/beauty", function (req, res) {
+  res.send("뷰티용품 쇼핑 페이지임");
 });
 
-app.get("/", function (요청, 응답) {
-  응답.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.render("index.ejs");
 });
 
-app.get("/write", (요청, 응답) => {
-  응답.sendFile(__dirname + "/write.html");
+app.get("/write", (req, res) => {
+  res.render("write.ejs");
 });
 
-// 어떤 사람이 /add 경로로 POST 요청을 하면... ??를 해주세요
-app.post("/add", function (요청, 응답) {
-  응답.send("전송완료");
+// 어떤 사람이 /add 경로로 POST req을 하면... ??를 해주세요
+app.post("/add", function (req, res) {
+  res.send("전송완료");
   db.collection("counter").findOne(
     { name: "게시물갯수" },
     function (error, result) {
@@ -52,7 +53,7 @@ app.post("/add", function (요청, 응답) {
       let 총게시물갯수 = result.totalPost;
       // DB.post에 새게시물을 기록함
       db.collection("post").insertOne(
-        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
+        { _id: 총게시물갯수 + 1, 제목: req.body.title, 날짜: req.body.date },
         function (error, result) {
           console.log("저장완료");
           // counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야함(수정)
@@ -61,7 +62,7 @@ app.post("/add", function (요청, 응답) {
             { $inc: { totalPost: 1 } }, //inc: 1을 증가시킴.
             function (error, result) {
               console.log("삭제완료");
-              응답.status(200).send({ message: "성공했습니다" });
+              res.status(200).send({ message: "성공했습니다" });
             }
           );
         }
@@ -90,5 +91,16 @@ app.delete("/delete", function (req, res) {
   // res.body에 담겨온 게시물번호를 가진 글을 db에서 찾아 삭제해주세요
   db.collection("post").deleteOne(req.body, function (error, result) {
     console.log("삭제완료");
+    res.status(200).send({ message: "성공했습니다" });
   });
+});
+
+app.get("/detail/:id", function (req, res) {
+  db.collection("post").findOne(
+    { _id: parseInt(req.params.id) },
+    function (error, result) {
+      console.log(result);
+      res.render("detail.ejs", { data: result });
+    }
+  );
 });
